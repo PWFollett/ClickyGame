ac3e8030e19986d081757f87f645c956d96f283a
PWFollett <pwf592110@gmail.com>
2019-09-09 15:40:46 -0400

    commiting

--- clicky-app/src/App.js
+++ clicky-app/src/App.js
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Image from "./components/Image";
+import Img from "./components/Img.json"
+
+//image imports
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+import x from ""
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    picked: [],
+    correct: 0,
+    topscore: 0,
+    message: 'Click an image to begin'
+  };
+
+//Shuffle Array
+  shuffleArray = (array) => {
+    let imgArray = Img;
+    for (let i = imgArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [imgArray[i], imgArray[j]] = [imgArray[j], imgArray[i]];
+    }
+    return imgArray
+  }
+
+  pickImg = (name) => {
+    console.log("Clicked!!");
+    let picked = this.state.picked;
+    
+    if (picked.indexOf(name) === -1) {
+      this.setState({
+        picked: picked.concat(name),
+        correct: this.state.correct + 1,
+        topscore: this.state.correct + 1 > this.state.topscore ? this.state.correct + 1 : this.state.topscore,
+        message: "Correct: Good choice!" 
+      })
+      this.shuffleArray();
+    }
+    else {
+      this.setState({
+        message: "Already Selected Game Over, Replay?",
+        correct: 0,
+        picked: []
+      })
+    }
+  }
+
+  imgSwitch = (name) => {
+    switch (name) {
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      case "":
+        return `${}`
+      default:
+        return `${}`
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar correct={this.state.correct} topscore={this.state.topscore} message={this.state.message}/>
+        <Header />
+        <Main>
+          {this.shuffleArray(Img).map(image => (
+            <Image src={this.imgSwitch(image.name)} name={image.name} key={image.name} pickImg={this.pickImg}  />
+          ))}
+        </Main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
